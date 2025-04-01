539c3930cee3f1d3a43a887e998ae9395eea2f38
duongdx <duong.dinh.xuan998@gmail.com>
2025-04-01 10:44:51 +0700

    edit notify.go

--- src/notify/notify.go
+++ src/notify/notify.go
@@ -4,28 +4,47 @@ import (
 	"os"
 	"s3-object-reporter/reports"
 	"strings"
+	"sync"
 )
 
 type sendfunc func(reports.Report) error
 
 func Notify(report reports.Report) []error {
-	var errorList []error
 	notifyMethods := strings.Split(os.Getenv("NOTIFY_METHODS"), ",")
-
 	adapters := map[string]sendfunc{
 		"influxdb": PushToInfluxDB,
 		"smtp":     SMTP,
 		"teams":    SendToTeamsAlert,
 	}
 
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(notifyMethods)) // Buffered channel for errors
+
 	for _, method := range notifyMethods {
-		err := adapters[strings.TrimSpace(method)](report)
-		if err != nil {
-			if errorList == nil {
-				errorList = []error{}
-			}
-			errorList = append(errorList, err)
+		method = strings.TrimSpace(method)
+		if sendFunc, exists := adapters[method]; exists {
+			wg.Add(1)
+
+			go func(f sendfunc) {
+				defer wg.Done()
+				if err := f(report); err != nil {
+					errChan <- err // Send error to channel
+				}
+			}(sendFunc)
 		}
 	}
+
+	// Close channel once all Goroutines finish
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	// Collect errors from channel
+	var errorList []error
+	for err := range errChan {
+		errorList = append(errorList, err)
+	}
+
 	return errorList
 }
